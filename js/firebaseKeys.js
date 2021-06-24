5a3cdfca36063ac514cc964adba89753c1d64556
Fabián Cruz <fabiancruz3773@gmail.com>
2021-06-23 21:11:21 -0500

    stuff

---
+++ js/firebaseKeys.js
@@ -0,0 +1,11 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCwiAO-2S5cfIak-MB7QGwmq-f24--Wwp8",
+    authDomain: "chat-general-e0ee9.firebaseapp.com",
+    projectId: "chat-general-e0ee9",
+    storageBucket: "chat-general-e0ee9.appspot.com",
+    messagingSenderId: "1054893696258",
+    appId: "1:1054893696258:web:356d1e5e8e7b9cc6fa4e1b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
